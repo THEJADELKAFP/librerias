cf7cb3de94b6c87223863a7fd2300217fe610c32
JADELKA / JDT <144500434+THEJADELKAFP@users.noreply.github.com>
2023-12-15 05:52:08 -1100

    si

--- index.py
+++ index.py
@@ -1,20 +1,14 @@
 import windjdk as jdk
 
-def pepe():
-    print("pepe")
+def pepe(): # funcion que imprime PEPE
+    print("PEPE")
 
-# Crear la instancia de la ventana
-root = jdk.Ventana("400x400", "JADELKA", False)
+root = jdk.Ventana("400x400", "JADELKA", False) # Crear la instancia de la ventana
 
-# Crear la instancia del botón en la ventana
 boton = jdk.Boton(root.root, "BOTON XD", pepe )  # Accede a la ventana con root.root
 boton.CreateBoton()  # Llama a CreateBoton para mostrar el botón
 
 label = jdk.Label(root.root, text="jadelka", font="arial", fg="black") # Accede a la Label con root.root
 label.createLabel()  # Llama a CreateLbael para mostrar el Label
 
-label = jdk.Label(root.root, text="jadelka", font="arial", fg="black")
-label.createLabel()
-
-# Iniciar el bucle principal de tkinter
 root.root.mainloop()
